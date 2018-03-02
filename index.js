b604bfbc0abf7c95e021bc049f64a77bb0739a5e
Fraoui <adamefraoui415@gmail.com>
2018-03-02 16:42:45 +0100

    authentication

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //On récupère le module express
 import express from 'express';
 
+//On récupère le module jwt token authentication
+import jsonwebtoken from 'jsonwebtoken';
+//
 import * as bodyParser from 'body-parser';
 
 //On récupère le module users/route
@@ -9,6 +12,7 @@ import router from "./users/route";
 //Déclaration de l'application
 let app = express();
 
+//l'app utilisera body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
@@ -16,7 +20,7 @@ app.use(bodyParser.json())
 
 app.use('/users',router);
 
-//On se connect au port 8092
+//On se connect au port 8077
 app.listen('8077',() => {
 	console.log('Connecter au port 8077');
 });
--- users/route.js
+++ users/route.js
@@ -1,7 +1,8 @@
 //On importe le module express
 import express from 'express';
 
-
+//On import le module jwt token
+import jwt from 'jsonwebtoken';
 
 //Définition du Router
 let router = express.Router();
@@ -9,6 +10,17 @@ let router = express.Router();
 //Tableau qui contiendra les users
 let users = [];
 
+router.get('/api', (req,res) => {
+	res.send('Test message');
+});
+
+
+
+router.post('/api/posts', verifyToken, (req, res) => {  
+  	// verify a token symmetric - synchronous
+	var decoded = jwt.verify(token, 'shhhhh');
+	console.log(decoded.foo) // bar
+	});
 
 router.post('/signup',(req,res) => {
 	let user = req.body;
@@ -27,32 +39,60 @@ router.post('/signup',(req,res) => {
 	} 
 	else{
 		res.send("Username ou password incorrect");
-	}  
+	} 
 });
 
 
-router.post('/login', (req,res) =>{
-	let user = req.body;
-	if(user.username && user.password){
-		var verifyUser = users.find(function(element){
-			return element.username == user.username && element.password == user.password;
-		})
-		if(verifyUser){
-			res.send("log");
-		}
-		else{
-			res.send("Username ou password incorrect");
-		}
+
+router.post('/login',(req,res) =>{
+
+	if(req.body.username && req.body.password) {
+			// sign with default (HMAC SHA256)
+			var jwt = require('jsonwebtoken');
+			var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+			//backdate a jwt 30 seconds
+			var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) +( 60 * 60) }, 'shhhhh');
+
+            console.log("Ceci est le token " +token);
+			
+		 res.send("Bienvenue cher utilisateur");
+
+
+	} else {
+
+		 res.send("Qui etes vous !?");
 	}
-	else{
-		res.send("Username ou password incorrect");
-	}
+
+	
 
 })
 
+
 router.get('/',(req,res) => {
    res.send(users);
+
 });
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
 
 //export du module router
 export default router;
